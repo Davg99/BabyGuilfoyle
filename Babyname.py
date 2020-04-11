5de9edeacd286dc8071499d0aec7cb3d3ce53f80
David Guilfoyle <DMGuilfoyle@gmail.com>
2020-04-10 18:09:50 -0600

    Added Favorite Book to Babyname.py

--- Babyname.py
+++ Babyname.py
@@ -6,6 +6,7 @@ FavColor = []
 BestSport = []
 FavCity = []
 FavBeer = []
+FavBook = []
 
 Username = input("What is your name?")
 BabyName = input("what should we name Baby Boy?")
@@ -13,5 +14,5 @@ FavColor = input("Wht will " + BabyName + "'s favorite color be?")
 BestSport = input("What will " + BabyName + "'s preferred sport be?")
 FavCity = input("What will " + BabyName + "'s favorite city be?")
 FavBeer = input("What will " + BabyName + "'s favorite beer be?")
-
+FavBook = input("What will " + BabyName + "'s favorite book be?")
 print("Awesome! Thanks " + Username + ".  " + BabyName + " will like " + BestSport + " visiting " + FavCity + "for a cold " + FavBeer + " while enjoying the color " + FavColor + "!")
