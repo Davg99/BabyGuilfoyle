11254dca585f12b3718e443246992ef92572d84a
Davg99 <63259602+Davg99@users.noreply.github.com>
2020-08-06 10:18:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,210 @@
+Home
+The Complete Web Developer Course 2.0
+Leave a rating
+Your progress
+Play video
 
-<?php 
-
-	mysqli_connect
-
-?>	
 
 
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8"><meta name="Baby Guilfoyle" content="width=device-width, initial-scale=1">
-	<title>Baby Guilfoyle</title>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	<link href="GIT/jquery-ui.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&amp;display=swap" rel="stylesheet" />
-	<link rel="stylesheet" type="text/css" href="/GIT/babyg.css">
-	
-</head>
-	<body>
-		<!-- Topbar Navigation CSS topbar, partition and topbar-menu -->
-		<div id="topbar">
-			<div class="partition topbar-menu"><a href="http://www.bootsoles.com" hrefs="http://www.bootsoles.com&quot;">Home</a></div>
-			<div class="partition topbar-menu"><a href="/GIT/Us.html" hrefs="http://www.bootsoles.com/Us&quot;">Us</a></div>
-			<div class="partition topbar-menu"><a href="/GIT/Kristin.html" hrefs="http://www.bootsoles.com/Kristin&quot;">Kristin</a></div>
-			<div class="partition topbar-menu"><a href="/GIT/David.html" hrefs="http://www.bootsoles.com/David&quot;">David </a></div>
-			<div class="partition topbar-menu"><a href="/GIT/Baby_Name.html" hrefs="http://www.bootsoles.com/Baby_Name&quot;">Baby Name </a></div>
-			<div class="partition topbar-menu"><a href="/GIT/Baby_Pics.html" hrefs="http://www.bootsoles.com/Baby_Pics&quot;">Baby Pics </a></div>
-		</div>
-		
 
-		<!-- Centered font and pictures-->
-	<div class="lead">
-		<h1><span style="font-size:72px;">Baby Guilfoyle</span></h1>
-		<p><span style="font-size:36px;">We are having a baby boy !<br /> We need help with his name .<br /><a href="/GIT/Baby_Name.html" hrefs="http://www.bootsoles.com/Baby_Name&quot;">Fill out the Baby Name Form  .</a></span></p>
-		<p><a href="/GIT/Us.html" hrefs="http://www.bootsoles.com/Us&quot;" target="_self"><img alt="Us" src="/GIT/Images/KD1.jpg" style="width: 160; height: 240;" /></a></p>
-	</div>	
-	<div class="row">
-	<div class="column" style="text-align: center;">
-		<a href="/GIT/Kristin.html" hrefs="http://www.bootsoles.com/Kristin&quot;" target="_self"><img alt="Kristin" src="GIT/Images/Kristin 1.jpg" style="width: 375px; height: 323px;" /></a> 
-		<a href="/GIT/David.html" hrefs="http://www.bootsoles.com/David&quot;"><img alt="David" src="GIT/Images/David 1.jpg" style="width: 375px; height: 323px;" /></a><a href="/GIT/Kristin.html" hrefs="http://www.bootsoles.com/Kristin&quot;" target="_self"> </a>
-	</div>
-		<a hrefs="http://www.bootsoles.com/David&quot;"> </a></div>
-	<div class="lead">
-		<p>Click for more from the pics above...</p>
-	</div>
 
-	<hr />
-		<p><a hrefs="http://www.bootsoles.com/David&quot;"> </a></p>
-		<p><a hrefs="http://www.bootsoles.com/David&quot;"> </a></p>
+1.5
+5:25 / 7:21
 
-	<footer>
-		<p>&copy; Baby Guilfoyle 2020</p>
-	</footer>
-	</body>
-</html>
 
+Overview
+Q&A
+Notes
+Announcements
+Click the "Create a new note" box, the "+" button, or press "N" to make your first note.
+Course content
+Section 1: Getting Started
+2 / 6|17min
+Section 2: HTML 5
+27 / 40|2hr 9min
+Section 3: CSS 3
+29 / 40|3hr 46min
+Section 4: Javascript
+22 / 37|2hr 42min
+Section 5: jQuery
+17 / 33|3hr 24min
+Section 6: Bootstrap 4
+1 / 21|2hr 15min
+Section 7: Wordpress
+0 / 11|1hr
+Section 8: PHP
+14 / 29|2hr 36min
+
+183. Introduction To PHP Section
+1min
+
+184. Introduction To PHP
+4min
+
+185. Hello World With PHP
+10min
+
+186. Hello World With PHP - Files
+1min
+
+187. Variables
+8min
+
+188. Variables - Files
+1min
+
+189. Arrays
+9min
+
+190. Arrays - Files
+1min
+
+191. If Statements
+5min
+
+192. If Statements - Files
+1min
+
+193. For And For Each Loops
+9min
+
+194. For And For Each Loops - Files
+1min
+
+195. While Loops
+5min
+
+196. While Loops - Files
+1min
+
+197. GET Variables
+18min
+
+198. GET Variables - Files
+1min
+
+199. POST Variables
+8min
+
+200. POST Variables - Files
+1min
+
+201. Sending An Email With PHP
+7min
+
+202. Sending An Email With PHP - Files
+1min
+
+203. Mini Project - A Contact Form
+30min
+
+204. Mini Project - A Contact Form - Files
+1min
+
+205. Getting Contents Of Other Scripts
+4min
+
+206. Getting Contents Of Other Scripts - Files
+1min
+
+207. Project - Weather Scraper
+37min
+
+208. Project - Weather Scraper - Files
+1min
+
+209. Further Reading - PHP
+1min
+
+Quiz 7: PHP - Quiz
+
+210. CCA PHP Level 1
+1min
+Section 9: MySQL
+2 / 24|2hr 52min
+
+211. Introduction To MySQL 5 Section
+1min
+
+212. Introduction To MySQL
+10min
+
+213. Connecting To A Database
+7min
+
+214. Connecting To A Database - Files
+1min
+
+215. Retrieving Data From A Database
+8min
+
+216. Retrieving Data From A Database - Files
+1min
+
+217. Inserting And Updating Data
+9min
+
+218. Inserting And Updating Data - Files
+1min
+
+219. Looping Through Data
+24min
+
+220. Looping Through Data - Files
+1min
+
+221. Session Variables
+8min
+
+222. Session Variables - Files
+1min
+
+223. Cookies
+6min
+
+224. Cookies - Files
+1min
+
+225. Storing Passwords Securely
+8min
+
+226. Storing Passwords Securely - Files
+1min
+
+227. A note about secure passwords - password_hash()
+1min
+
+228. Project Secret Diary (1)
+44min
+
+229. Project Secret Diary - Part 1 Files
+1min
+
+230. Project - Secret Diary (2)
+47min
+
+231. Project Secret Diary - Part 2 Files
+1min
+
+232. Further Reading - MySQL
+1min
+
+Quiz 8: MySQL - Quiz
+
+233. CCA MySQL Level 1
+1min
+Section 10: APIs
+0 / 17|1hr 39min
+Section 11: Mobile Apps
+0 / 12|1hr 54min
+Section 12: HTML 5 & CSS 3 Special Features
+0 / 24|2hr 11min
+Section 13: Python
+0 / 16|1hr 29min
+Section 14: Bonus Section: Twitter Clone Using MVC
+0 / 6|2hr 23min
+Section 15: Where do you go from here?
+0 / 1|1min
+Udemy for Business Copyright © 2020 Udemy, Inc. Udemy app About us Privacy Policy Terms Help and Support Sitemap
+
+×Close alert
