de0cea8a21265339688f7881e5b5d6b62a51c8c4
Davg99 <63259602+Davg99@users.noreply.github.com>
2020-08-06 17:02:35 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,55 @@
 
 <?php 
 
-mysqli_connect("localhost:3306", "bootso5_Davg99", "Iceland18!");
+if (array_key_exists('name', $_POST)) {
+    
+$link = mysqli_connect("localhost", "bootso5_Davg99", "Iceland18!", "bootso5_Baby_Name");
 
 if (mysqli_connect_error()) {
 
-    echo "There was an error connecting to the database";
+    die ("There was an error connecting to the database");
 
-} else {
+} 
+    if ($_POST['name'] == '') {
 
-    echo "Database connection succesful";
+        echo "The Name is blank!";
+    } else {
+
+        $query = "SELECT `id` FROM `BabyName` WHERE name = '".mysqli_real_escape_string($link, $_POST['name'])."'";   
+
+        $result = mysqli_query($link, $query);
+        
+        if (mysqli_num_rows($result) > 0) {
+
+            echo "That Name already exists";
+        } else { 
+            
+            $query = "INSERT INTO `BabyName` (`name`) VALUES ('".mysqli_real_escape_string($link, $_POST['name'])."')";
+
+            if (mysqli_query($link, $query)) {
+
+                echo "We got the name.";
+
+        } else {
+
+            echo "Nope";  
+        }
+    }
+
 }
 
+}
+
+
 ?>	
+
+<form method = "post">  
+
+<h2>Baby Form</h2>
+
+<input name="name" type="text" placeholder = "What should the baby be named?" />
+
+<input type="submit" id="submitButton" value="Submit Form" /> 
+
+
+</form>
