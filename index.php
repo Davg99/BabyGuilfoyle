7a3a1f0995057667bfdd5cbdd1828dc0559269a1
Davg99 <63259602+Davg99@users.noreply.github.com>
2020-08-06 10:14:06 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<?php 
+
+	mysqli_connect
+
+?>	
+
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"><meta name="Baby Guilfoyle" content="width=device-width, initial-scale=1">
+	<title>Baby Guilfoyle</title>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<link href="GIT/jquery-ui.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&amp;display=swap" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="/GIT/babyg.css">
+	
+</head>
+	<body>
+		<!-- Topbar Navigation CSS topbar, partition and topbar-menu -->
+		<div id="topbar">
+			<div class="partition topbar-menu"><a href="http://www.bootsoles.com" hrefs="http://www.bootsoles.com&quot;">Home</a></div>
+			<div class="partition topbar-menu"><a href="/GIT/Us.html" hrefs="http://www.bootsoles.com/Us&quot;">Us</a></div>
+			<div class="partition topbar-menu"><a href="/GIT/Kristin.html" hrefs="http://www.bootsoles.com/Kristin&quot;">Kristin</a></div>
+			<div class="partition topbar-menu"><a href="/GIT/David.html" hrefs="http://www.bootsoles.com/David&quot;">David </a></div>
+			<div class="partition topbar-menu"><a href="/GIT/Baby_Name.html" hrefs="http://www.bootsoles.com/Baby_Name&quot;">Baby Name </a></div>
+			<div class="partition topbar-menu"><a href="/GIT/Baby_Pics.html" hrefs="http://www.bootsoles.com/Baby_Pics&quot;">Baby Pics </a></div>
+		</div>
+		
+
+		<!-- Centered font and pictures-->
+	<div class="lead">
+		<h1><span style="font-size:72px;">Baby Guilfoyle</span></h1>
+		<p><span style="font-size:36px;">We are having a baby boy !<br /> We need help with his name .<br /><a href="/GIT/Baby_Name.html" hrefs="http://www.bootsoles.com/Baby_Name&quot;">Fill out the Baby Name Form  .</a></span></p>
+		<p><a href="/GIT/Us.html" hrefs="http://www.bootsoles.com/Us&quot;" target="_self"><img alt="Us" src="/GIT/Images/KD1.jpg" style="width: 160; height: 240;" /></a></p>
+	</div>	
+	<div class="row">
+	<div class="column" style="text-align: center;">
+		<a href="/GIT/Kristin.html" hrefs="http://www.bootsoles.com/Kristin&quot;" target="_self"><img alt="Kristin" src="GIT/Images/Kristin 1.jpg" style="width: 375px; height: 323px;" /></a> 
+		<a href="/GIT/David.html" hrefs="http://www.bootsoles.com/David&quot;"><img alt="David" src="GIT/Images/David 1.jpg" style="width: 375px; height: 323px;" /></a><a href="/GIT/Kristin.html" hrefs="http://www.bootsoles.com/Kristin&quot;" target="_self"> </a>
+	</div>
+		<a hrefs="http://www.bootsoles.com/David&quot;"> </a></div>
+	<div class="lead">
+		<p>Click for more from the pics above...</p>
+	</div>
+
+	<hr />
+		<p><a hrefs="http://www.bootsoles.com/David&quot;"> </a></p>
+		<p><a hrefs="http://www.bootsoles.com/David&quot;"> </a></p>
+
+	<footer>
+		<p>&copy; Baby Guilfoyle 2020</p>
+	</footer>
+	</body>
+</html>
+
